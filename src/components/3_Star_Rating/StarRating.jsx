a5ae31ecb4df8c28e34599f0292714bb0edee08f
Bhushan Gawande <brgawande@gmail.com>
2024-07-26 00:24:09 +0530

    star rating done

--- src/components/3_Star_Rating/StarRating.jsx
+++ src/components/3_Star_Rating/StarRating.jsx
@@ -2,25 +2,41 @@ import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 const StarRating = ({ starsNumbers }) => {
-  const [rating, setRating] = useState(null);
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(null);
 
-  console.log(rating);
+  const ratingHandler = (getCurrentIndex) => {
+    setRating(getCurrentIndex);
+  };
+
+  const handleMouseEnter = (getCurrentIndex) => {
+    setHover(getCurrentIndex);
+  };
+  const handleMouseLeave = (getCurrentIndex) => {
+    setHover(rating);
+  };
 
   return (
     <div>
       <h1 className="text-3xl py-2 font-bold text-center">Star Rating</h1>
       <div className="ratings dflex gap-3 my-6">
-        {[...Array(starsNumbers)].map((item, index) => (
-          <FaStar
-            key={index}
-            onClick={() => setRating(index + 1)}
-            onMouseEnter={() => setRating(index + 1)}
-            onMouseLeave={() => setRating(index + 1)}
-            className={`${
-              rating >= index + 1 ? "text-yellow-300" : "text-black"
-            } text-3xl`}
-          />
-        ))}
+        {[...Array(starsNumbers)].map((item, index) => {
+          index += 1;
+          return (
+            <FaStar
+              key={index}
+              onClick={() => ratingHandler(index)}
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={() => handleMouseLeave(index)}
+              // className={`${
+              //   rating >= index + 1 ? "text-yellow-300" : "text-black"
+              // } text-3xl`}
+              className={`${
+                index <= (hover || rating) ? "text-yellow-300" : "text-black"
+              } text-3xl`}
+            />
+          );
+        })}
       </div>
     </div>
   );
